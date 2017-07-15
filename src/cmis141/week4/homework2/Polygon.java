ccccbb4fda3850b341f824d8f21803c0713e8ddc
BozoJim <JStokes7838@gmail.com>
2017-07-15 13:50:52 -0400

    privatization of test cases commented all code

--- src/cmis141/week4/homework2/Polygon.java
+++ src/cmis141/week4/homework2/Polygon.java
@@ -1,29 +1,40 @@
 package cmis141.week4.homework2;
 
+/*  File:    Polygon.java
+ *  Author:  Stokes, James
+ *  Date:    7/11/2017
+ *  Purpose: This class will accept a number of sides, side length, and x/y coordinates. There is a default constructor
+ *           for no parameters as well as a constructor for all parameters.
+ */
+
 /**
- * Created by 590905 on 7/10/2017.
+ * Create a polygon
  */
 public class Polygon {
 
+    // variable initialization
     private int numSides;
     private double sideLength, xCoord, yCoord, perimeter;
 
+    // no-argument constructor
     public Polygon() {
         this.numSides = 4;
         this.sideLength = 5.0;
         this.xCoord = 1.0;
         this.yCoord = 1.0;
-        this.perimeter = getPerimeter();
+        this.perimeter = getPerimeter(); // calculate and initialize perimeter based on method below
     }
 
+    // constructor which accepts all necessary parameters
     public Polygon(int numSides, double sideLength, double xCoord, double yCoord) {
         this.numSides = numSides;
         this.sideLength = sideLength;
         this.xCoord = xCoord;
         this.yCoord = yCoord;
-        this.perimeter = getPerimeter();
+        this.perimeter = getPerimeter(); // calculate and initialize perimeter based on method below
     }
 
+    // number of sides getter and setter
     public int getNumSides() {
         return numSides;
     }
@@ -32,6 +43,7 @@ public class Polygon {
         this.numSides = numSides;
     }
 
+    // side length getter and setter
     public double getSideLength() {
         return sideLength;
     }
@@ -40,6 +52,7 @@ public class Polygon {
         this.sideLength = sideLength;
     }
 
+    // x-coord getter and setter
     public double getxCoord() {
         return xCoord;
     }
@@ -48,6 +61,7 @@ public class Polygon {
         this.xCoord = xCoord;
     }
 
+    // y-coord getter and setter
     public double getyCoord() {
         return yCoord;
     }
@@ -56,11 +70,13 @@ public class Polygon {
         this.yCoord = yCoord;
     }
 
+    // initialize perimeter. called during constructor.
     public double getPerimeter() {
         perimeter = getNumSides() * getSideLength();
         return perimeter;
     }
 
+    // toString method for basic output of polygon information
     @Override
     public String toString() {
         return "Polygon{" +
@@ -72,4 +88,4 @@ public class Polygon {
                 '}';
     }
 
-}
+} // end Polygon
