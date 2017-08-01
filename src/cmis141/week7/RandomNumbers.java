46eae75720b9815f7a9e3e455f1ffe485af6c6ad
James Stokes <JStokes7838@gmail.com>
2017-07-31 22:35:26 -0400

    added a method for sorting.

--- src/cmis141/week7/RandomNumbers.java
+++ src/cmis141/week7/RandomNumbers.java
@@ -1,5 +1,7 @@
 //package cmis141.week7;
 
+import java.util.Arrays;
+
 /**
  * File: RandomNumbers.java
  * Author: James Stokes
@@ -12,9 +14,14 @@ public class RandomNumbers {
         int[] randomArray = new int[x];
 
         for (int i=0; i<x; i++) {
-            randomArray[i] = (int)(Math.random() * y);
+            randomArray[i] = (int)(Math.random() * (y+1));
         }
         return randomArray;
+    }
+
+    private int[] sortedArray(int[] array) {
+        Arrays.sort(array);
+        return array;
     }
 
     private String printRandomNumber(int iteration, int number) {
@@ -22,14 +29,27 @@ public class RandomNumbers {
     }
 
     public static void main(String[] args) {
+        // collect variables from command line arguments as x and y.
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
 
-        RandomNumbers arrayOfInts =  new RandomNumbers();
-        int[] myArray = arrayOfInts.RandomInts(x, y);
+        // create an array of random numbers
+        int[] myArray =  new RandomNumbers().RandomInts(x,y);
 
+        System.out.println("Random Array:");
+        // print each random number in the array
         for (int i = 0; i < myArray.length; i++) {
-            System.out.println(arrayOfInts.printRandomNumber(i, myArray[i]));
+            System.out.println(new RandomNumbers().printRandomNumber(i, myArray[i]));
         }
+        System.out.println();
+
+        // sort the same array of numbers
+        int[] sortedArray = new RandomNumbers().sortedArray(myArray);
+        System.out.println("Sorted Numbers:");
+        // print the new sorted array
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println(new RandomNumbers().printRandomNumber(i, sortedArray[i]));
+        }
+
     }
 }
